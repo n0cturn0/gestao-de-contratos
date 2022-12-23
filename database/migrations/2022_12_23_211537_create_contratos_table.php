9b2e8b86759d5712fe366cf1980d7b94299ccb17
Luiz Augusto <n0cturn0.debian@gmail.com>
2022-12-23 17:20:08 -0400

    Created model and migrate for Contratos

---
+++ database/migrations/2022_12_23_211537_create_contratos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContratosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contratos', function (Blueprint $table) {
+            $table->id();
+            $table->integer('idCliente');
+            $table->integer('idProduto');
+            $table->string('observacao')->comment('Campo não obrigatório');
+            $table->integer('status')->comment('0 = Ativo 1 = Inativo')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contratos');
+    }
+}
