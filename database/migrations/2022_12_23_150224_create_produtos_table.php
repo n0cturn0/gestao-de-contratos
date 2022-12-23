7ad17ceb15ffee0e574dac1a61196fd1b36307e5
Luiz Augusto <n0cturn0.debian@gmail.com>
2022-12-23 11:09:28 -0400

    Created Model for produtos

---
+++ database/migrations/2022_12_23_150224_create_produtos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProdutosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id();
+            $table->string('produto')->comment('Nome do produto');
+            $table->integer('status')->comment('0 = Ativo 1 = Inativo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+}
