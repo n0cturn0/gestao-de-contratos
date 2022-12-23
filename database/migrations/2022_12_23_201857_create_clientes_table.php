4bdd99dc498a840d5eec1e6cde82123dd732b367
Luiz Augusto <n0cturn0.debian@gmail.com>
2022-12-23 16:22:52 -0400

    Created model e migrate Cliente

---
+++ database/migrations/2022_12_23_201857_create_clientes_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('cliente')->comment('Nome do cliente');
+            $table->string('cnpj')->comment('Cnpj');
+            $table->string('estado');
+            $table->string('cidade');
+            $table->string('bairro');
+            $table->string('rua');
+            $table->integer('numero');
+            $table->integer('status')->comment('0 = Ativo 1 = Inativo')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+}
