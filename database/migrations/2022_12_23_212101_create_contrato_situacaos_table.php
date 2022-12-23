50f3101c001bc669fe624f0bf78dc061a03ca2bf
Luiz Augusto <n0cturn0.debian@gmail.com>
2022-12-23 17:25:41 -0400

    Created Model and migrate que vai contratar Situacao do contrado

---
+++ database/migrations/2022_12_23_212101_create_contrato_situacaos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContratoSituacaosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contrato_situacaos', function (Blueprint $table) {
+            $table->id();
+            $table->integer('idcontrato');
+            $table->integer('situacao')->comment('0 = Ativo 1 = Inativo 2= Renovado 3= Reincidido');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contrato_situacaos');
+    }
+}
