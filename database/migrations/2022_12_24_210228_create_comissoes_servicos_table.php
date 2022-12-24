f77915712d9bae2f1bf4f3f6aa7c990407f91a53
Luiz Augusto <n0cturn0.debian@gmail.com>
2022-12-24 17:06:49 -0400

    Created Model e migrates para controle de comissões

---
+++ database/migrations/2022_12_24_210228_create_comissoes_servicos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComissoesServicosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comissoes_servicos', function (Blueprint $table) {
+            $table->id();
+            $table->integer('idproduto');
+            $table->integer('idservico');
+            $table->string('porcentagem');
+            $table->string('valorservico');
+            $table->string('valorfinal');
+            $table->integer('status')->comment('0 = Ativo 1 = Inativo')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comissoes_servicos');
+    }
+}
