d5229121e63b9a4eb4691d7ad98ef1243f5ef71c
Luiz Augusto <n0cturn0.debian@gmail.com>
2023-02-24 10:56:44 -0400

    Correção de label, que exibia texto errado

--- resources/views/livewire/vendedormodal.blade.php
+++ resources/views/livewire/vendedormodal.blade.php
@@ -5,7 +5,7 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="studentModalLabel">Cadastrar novo cliente</h5>
+                <h5 class="modal-title" id="studentModalLabel">Cadastrar novo vendedor</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"
                         wire:click="closeModal"></button>
             </div>
