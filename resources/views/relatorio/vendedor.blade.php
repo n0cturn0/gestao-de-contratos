8a80dfb71f23ff3ded3e82f51d008ba683354909
Luiz Augusto <n0cturn0.debian@gmail.com>
2023-04-12 16:14:39 -0400

    Adicionado checkbox de filtros

--- resources/views/relatorio/vendedor.blade.php
+++ resources/views/relatorio/vendedor.blade.php
@@ -32,11 +32,17 @@
 
                         @endif
 
+                        <div class="card" style="margin-top: 25px;">
+                            <div class="card-header">
+                                <h4>Relatório por vendedor</h4>
+                            </div>
                         <div class="card-body">
+                            <form method="POST" action="/relvendedor">
+                                @csrf
                             <div class="form-group">
                                 <div class="row">
-
-                                        <div class="col-md-3">
+                                        <div class="col-md-6">
+                                            <label>Selecione o período </label>
                                             <div class="input-group date" id="reservationdate" data-target-input="nearest">
                                                 <input type="text" class="form-control" name="relatorio" value="01/01/2018 - 01/15/2018" />
                                                 <div class="input-group-append" data-target="#reservationdate" data-toggle="datetimepicker">
@@ -46,30 +52,36 @@
                                         </div>
 
 
-                                    <div class="col-md-3">
-                                        <div class="input-group date" id="reservationdate" data-target-input="nearest">
-                                            <input type="text" class="form-control" name="relatorio" value="01/01/2018 - 01/15/2018" />
-                                            <div class="input-group-append" data-target="#reservationdate" data-toggle="datetimepicker">
-                                                <div class="input-group-text"><i class="fa fa-calendar"></i></div>
-                                            </div>
+                                    <div class="col-md-6">
+                                        <label>Selecione o vendedor</label>
+                                            <select name="vendedor" class="custom-select form-control-border" id="exampleSelectBorder">
+                                                <option value="">-----</option>
+                                                @foreach($vendedor as  $value)
+                                                <option value="{{$value->id}}">{{$value->vendedor}}</option>
+                                                @endforeach
+                                            </select>
                                         </div>
                                     </div>
-
-
-
-
-
-
-
-
-
-
                                 </div>
-                            </div>
+                                <div class="row">
+                                <div class="form-group">
+                                    <div class="icheckbox_flat-green checked" style="position: relative;" aria-checked="true" aria-disabled="false">
+                                        <input type="checkbox" class="flat-red" >Pendentes
+                                        <input type="checkbox" class="flat-red" >Pagas
+                                        <input type="checkbox" class="flat-red" >Vencidas
+
+                                    </div>
+                                </div>
+
+
 
                             <div class="row">
-                                <button type="submit" class="btn btn-block btn-primary btn-xs">Atualizar para vendedor selecionado</button>
+                                <button type="submit" class="btn btn-block btn-primary btn-xs">Gerar Relatório</button>
                             </div>
+                            </div>
+                        </form>
+                        </div>
+
 
                         </div>
                     </div>
