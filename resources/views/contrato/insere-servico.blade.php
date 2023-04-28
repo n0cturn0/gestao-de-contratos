b588c3b74713ffd5901a02a7f29e11d4941328e7
Luiz Augusto <n0cturn0.debian@gmail.com>
2023-04-27 23:10:55 -0400

    Remoção de comentário

--- resources/views/contrato/insere-servico.blade.php
+++ resources/views/contrato/insere-servico.blade.php
@@ -10,7 +10,7 @@
 @stop
 
 @section('content')
-{{--    @livewire('contrato.gerador')--}}
+
 
 
 <div>
@@ -26,10 +26,7 @@
                     <div class="card" style="margin-top: 25px;">
                         <div class="card-header">
                             <h4>
-                                {{--                                <input type="search" wire:model="search" class="form-control float-end mx-2" placeholder="Buscar cliente" style="width: 230px" />--}}
-                                {{--                                <button type="button" class="btn btn-primary float-end" data-bs-toggle="modal" data-bs-target="#studentModal">--}}
-                                {{--                                    Adicionar novo Vendedor--}}
-                                {{--                                </button>--}}
+
                             </h4>
                         </div>
 
